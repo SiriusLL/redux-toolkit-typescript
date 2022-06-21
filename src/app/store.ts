5fc6b1c7ebb6d02fd2637e3487c9021c9257e43d
SiriusLL <Sirius.love.light@gmail.com>
2022-06-21 00:13:33 -0600

    create reservationsSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,11 @@
 import { configureStore, Store } from "@reduxjs/toolkit";
-
+import reservationsReducer from "../features/reservationSlice";
 
 export const store = configureStore({
   reducer: {
+    reservations: reservationsReducer,
+  },
+});
 
-  }
-})
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
